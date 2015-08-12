ef83eb4fcec0741df816127276f2f7bb44e9900c
nimish <nimish.gupta@vinsol.com>
2015-08-13 00:01:40 +0530

    factorial using ranges

---
+++ factorial-ranges/bin/main.rb
@@ -0,0 +1,4 @@
+require_relative '../lib/fixnum'
+
+number = gets.chomp.to_i
+p number.calculate_factorial
---
+++ factorial-ranges/lib/fixnum.rb
@@ -0,0 +1,7 @@
+class Fixnum
+
+  def calculate_factorial
+    (1..self).inject(:*)
+  end
+
+end
