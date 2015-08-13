b3f8f1c1e6c9ab6d5787b0b73ceafb7ce2448415
nimish <nimish.gupta@vinsol.com>
2015-08-13 17:39:32 +0530

    fix indentation issues

--- count-ranges/lib/string.rb
+++ count-ranges/lib/string.rb
@@ -2,18 +2,18 @@ require_relative 'hash'
 
 class String
 
-def count_type_of_characters
+  def count_type_of_characters
     count = Hash.new(0)
     each_char do |character|
       case character
       when '0'..'9'
-        count.increment_value(:no_of_digit, 1 )
+        count.increment_value(:no_of_digit, 1)
       when 'a'..'z'
-        count.increment_value(:no_of_lowercase, 1 )
+        count.increment_value(:no_of_lowercase, 1)
       when 'A'..'Z'
-        count.increment_value(:no_of_uppercase, 1 )
+        count.increment_value(:no_of_uppercase, 1)
       else
-        count.increment_value(:no_of_special_characters, 1 )
+        count.increment_value(:no_of_special_characters, 1)
       end
     end
     count
