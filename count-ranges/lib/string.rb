6d78601b2238f7e5abd057cbacf796bbe1ae67d6
nimish <nimish.gupta@vinsol.com>
2015-08-13 12:54:07 +0530

    modify count occurence of different alphabet

--- count-ranges/lib/string.rb
+++ count-ranges/lib/string.rb
@@ -7,13 +7,13 @@ def count_type_of_characters
     each_char do |character|
       case character
       when '0'..'9'
-        count.increment_value('no_of_digit', 1 )
+        count.increment_value(:no_of_digit, 1 )
       when 'a'..'z'
-        count.increment_value('no_of_lowercase', 1 )
+        count.increment_value(:no_of_lowercase, 1 )
       when 'A'..'Z'
-        count.increment_value('no_of_uppercase', 1 )
+        count.increment_value(:no_of_uppercase, 1 )
       else
-        count.increment_value('no_of_special_characters', 1 )
+        count.increment_value(:no_of_special_characters, 1 )
       end
     end
     count
