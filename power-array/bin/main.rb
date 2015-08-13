47762053c2a13c7e2db18b8efafe0fb97840144a
nimish <nimish.gupta@vinsol.com>
2015-08-13 13:12:08 +0530

    fix indentation in main.rb

--- power-array/bin/main.rb
+++ power-array/bin/main.rb
@@ -1,6 +1,6 @@
-require_relative "../lib/array.rb"
+require_relative '../lib/array'
 
-array = [1,2,3,4,5]
-print "Enter exponent value : "
+array = [1, 2, 3, 4, 5]
+print 'Enter exponent value : '
 exponent = gets.chomp.to_i
 p array.power(exponent)
