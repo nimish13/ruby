8a5e541f310aea748b2a318331c3796eded8cd61
nimish <nimish.gupta@vinsol.com>
2015-08-15 01:24:28 +0530

    find prime numbers using step

---
+++ prime-numbers/lib/fixnum.rb
@@ -0,0 +1,21 @@
+class Fixnum
+
+  def find_prime_numbers
+    if self > 1
+      array_of_prime_numbers = [2]
+    else
+      return 'No prime number found'
+    end
+    3.step(self, 2) do |number|
+      flag_value = true
+      (2..Math.sqrt(number) + 1).each do |value|
+        if number % value == 0
+          flag_value = false
+          break
+        end
+      end
+      array_of_prime_numbers << number if flag_value
+    end
+    array_of_prime_numbers
+  end
+end
