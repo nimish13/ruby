80dfd36eca80d9a9d209b8a4e7ae112288b12922
nimish <nimish.gupta@vinsol.com>
2015-08-17 18:19:37 +0530

    add access control

--- sum-time/lib/sum_time.rb
+++ sum-time/lib/sum_time.rb
@@ -8,13 +8,15 @@ class SumTime
 
   def time_parse(*string)
     return 'Invalid time format' if !TimeValidator.valid?(string)
-    time = string.map {|time| Time.parse(time)}
+    time = string.map { |time| Time.parse(time) }
     add_time(time)
   end
 
+  private
+
   def add_time(time)
     sum_time = time[0] + time[1].hour * HOUR_CONVERSION + time[1].min * MIN_CONVERSION + time[1].sec
-    string_conversion_of_time = "#{sum_time.hour}:" + "#{sum_time.min}:" + "#{sum_time.sec}"
+    string_conversion_of_time = "#{ sum_time.hour }:" + "#{ sum_time.min }:" + "#{ sum_time.sec }"
     time_prepend(string_conversion_of_time, time[0], sum_time)
   end
 
